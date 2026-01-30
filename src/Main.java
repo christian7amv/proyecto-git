97d27a6c1ff751f0313ede4af34fb5c436c11d80
Christian <christian7amv@gmail.com>
2026-01-30 12:43:26 +0100

    Cambio en la rama beta

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo desde Git y Java");
         System.out.println("Presentacion del Proyecto Inter Modular");
+        System.out.println("Cambio realizado desde la rama beta");
     }   
 
 }
