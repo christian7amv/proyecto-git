d4f8943242967d8eb50941d51fee3614638c736d
Christian <christian7amv@gmail.com>
2026-01-30 12:15:53 +0100

    añadimos la presentacion del proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo desde Git y Java");
-    }
+        System.out.println("Presentacion del Proyecto Inter Modular");
+    }   
+
 }
